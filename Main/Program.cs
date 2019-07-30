d54a85458510584a9a282e543b2fccff25d0251f
skdbs <choicnyy@gmail.com>
2019-07-30 15:54:13 +0900

    Add project files.

---
+++ Main/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    class Program { static void Main(string[] args) { System.Console.WriteLine("version 1"); } }
+
+}
